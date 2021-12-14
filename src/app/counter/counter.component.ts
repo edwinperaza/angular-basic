8549579884a48e1568faf4139c82ae016bdaf873
Edwin Peraza <edwinperaza87@gmail.com>
2021-12-14 00:00:52 -0500

    Add Heroe component manually

---
+++ src/app/counter/counter.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core'
+
+@Component({
+    selector: 'app-counter',
+    template: `
+
+        <h1>{{ title }}</h1>
+        <h3>La base es: <strong> {{ base }}</strong></h3>
+
+        <button (click)="acumular(base)"> + {{base}} </button>
+        <span> {{ myNumber }} </span>
+        <button (click)="acumular(-base)"> - {{base}} </button>
+    
+    `
+})
+export class CounterComponent {
+    title: string = 'Contador App';
+    myNumber: number = 10;
+    base: number = 5;
+  
+    acumular( valor: number) {
+      this.myNumber += valor;
+    }
+}
